df6aabcc3a0cea5feaa3d65d8904efe06355b56f
Nick von Kaenel <52364921+nickvonkaenel@users.noreply.github.com>
2024-09-09 12:21:21 -0700

    adding .dat to files associations

--- init.lua
+++ init.lua
@@ -645,6 +645,11 @@ require('lazy').setup({
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
             },
+            files = {
+              associations = {
+                ["*.dat"] = "lua",
+              },
+            },
           },
         },
       }
