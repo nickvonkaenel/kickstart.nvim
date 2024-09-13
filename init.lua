ae338b8a3febab14760140b4a5b16478b108538d
NickvonKaenel <52364921+NickvonKaenel@users.noreply.github.com>
2024-09-13 15:19:14 -0700

    noice and change lazygit command to just l for now

--- init.lua
+++ init.lua
@@ -326,6 +326,7 @@ require('lazy').setup({
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>l', group = '[L]azygit' },
       },
     },
   },
