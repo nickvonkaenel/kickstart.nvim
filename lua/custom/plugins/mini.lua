70a9d78288671d72e6db3afbc7f6dc267cfd6c14
NickvonKaenel <52364921+NickvonKaenel@users.noreply.github.com>
2024-09-23 18:04:12 -0700

    avante and moving plugins into folder

---
+++ lua/custom/plugins/mini.lua
@@ -0,0 +1,23 @@
+return { -- Collection of various small independent plugins/modules
+  'echasnovski/mini.nvim',
+  config = function()
+    -- Better Around/Inside textobjects
+    --
+    -- Examples:
+    --  - va)  - [V]isually select [A]round [)]paren
+    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+    --  - ci'  - [C]hange [I]nside [']quote
+    require('mini.ai').setup {
+      custom_textobjects = {
+        F = require('mini.ai').gen_spec.treesitter { a = '@function.outer', i = '@function.inner' },
+      },
+      n_lines = 500,
+    }
+    -- Add/delete/replace surroundings (brackets, quotes, etc.)
+    --
+    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+    -- - sd'   - [S]urround [D]elete [']quotes
+    -- - sr)'  - [S]urround [R]eplace [)] [']
+    require('mini.surround').setup()
+  end,
+}
