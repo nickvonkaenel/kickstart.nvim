5c597132c1b24427755448749bf3ca7472cf31a0
NickvonKaenel <52364921+NickvonKaenel@users.noreply.github.com>
2024-10-01 17:42:47 -0700

    mini

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -26,7 +26,67 @@ return { -- Collection of various small independent plugins/modules
       },
     }
 
-    vim.keymap.set('n', '<leader>f', '<cmd>lua MiniFiles.open()<CR>', { desc = '[F]iles' })
+    local minifiles_toggle = function(...)
+      if not MiniFiles.close() then
+        MiniFiles.open(...)
+      end
+    end
+    vim.keymap.set('n', '<leader>f', minifiles_toggle, { desc = '[F]iles' })
     vim.api.nvim_set_hl(0, 'MiniFilesCursorLine', { link = 'NONE' }) -- remove cursorline highlight
+
+    -- Open file in split window
+    local map_split = function(buf_id, lhs, direction)
+      local rhs = function()
+        -- Make new window and set it as target
+        local cur_target = MiniFiles.get_explorer_state().target_window
+        local new_target = vim.api.nvim_win_call(cur_target, function()
+          vim.cmd(direction .. ' split')
+          return vim.api.nvim_get_current_win()
+        end)
+
+        MiniFiles.set_target_window(new_target)
+      end
+
+      -- Adding `desc` will result into `show_help` entries
+      local desc = 'Split ' .. direction
+      vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+    end
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesBufferCreate',
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        -- Tweak keys to your liking
+        map_split(buf_id, '<C-s>', 'belowright horizontal')
+        map_split(buf_id, '<C-v>', 'belowright vertical')
+      end,
+    })
+
+    -- Set working directory to the file system entry under cursor
+    local files_set_cwd = function(path)
+      -- Works only if cursor is on the valid file system entry
+      local cur_entry_path = MiniFiles.get_fs_entry().path
+      local cur_directory = vim.fs.dirname(cur_entry_path)
+      vim.fn.chdir(cur_directory)
+    end
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesBufferCreate',
+      callback = function(args)
+        vim.keymap.set('n', '<C-t>', files_set_cwd, { buffer = args.data.buf_id, desc = 'Set working directory' })
+      end,
+    })
+    -- Makes the window transparent
+    -- vim.api.nvim_create_autocmd('User', {
+    --   pattern = 'MiniFilesWindowOpen',
+    --   callback = function(args)
+    --     local win_id = args.data.win_id
+    --
+    --     -- Customize window-local settings
+    --     vim.wo[win_id].winblend = 20
+    --     local config = vim.api.nvim_win_get_config(win_id)
+    --     vim.api.nvim_win_set_config(win_id, config)
+    --   end,
+    -- })
   end,
 }
